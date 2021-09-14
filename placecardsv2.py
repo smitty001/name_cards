2beb26f374d82cb3059d3817ca1877dc76d09901
smitty001 <32442733+smitty001@users.noreply.github.com>
2021-09-13 20:54:02 -0600

    done
    done

--- placecardsv2.py
+++ placecardsv2.py
@@ -1,4 +1,5 @@
 from typing import Counter
+import sys
 from reportlab.lib.pagesizes import A4
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.lib.units import mm
@@ -18,11 +19,15 @@ from rotatedtext import verticalText
 
 width = 5
 height = 3
-font_size = 18
+font_size = 26
 font = 'Ace'
 card_spacing = 0
-spacing = 19
-adjuster = 2.1
+spacing = 12 # smaller is a bigger space when multi-line
+adjuster = 1.55 # spacing for 180 rotations
+
+# get arguments
+if len(sys.argv) == 2:
+    font_size = int(sys.argv[1])
 
 # Read in the CSV
 def get_file():
@@ -47,7 +52,7 @@ def return_array(num, div):
 
 def setCan(canvas):
     canvas.setFont(font, font_size)
-    canvas.setLineWidth(.1)
+    canvas.setLineWidth(.02)
 
 
 def generate_pdf(names):
@@ -58,21 +63,25 @@ def generate_pdf(names):
     PAGE_HEIGHT = defaultPageSize[1]
 
     width_array = return_array(PAGE_HEIGHT, width)
-    height_array = return_array(PAGE_WIDTH, height)
+    #height_array = return_array(PAGE_WIDTH, height)
+    height_array = [PAGE_WIDTH/4, 3 * (PAGE_WIDTH/4)]
 
     width_line = return_array(PAGE_HEIGHT, width*4)
     width_line = width_line[::2]
     height_line = return_array(PAGE_WIDTH, height*2)
-    height_line = height_line[::2]
+    #height_line = height_line[::2]
 
 
     def drawLine(can):
         # Draw grid lines
-        flipper = True
+        # center line
+        can.line(PAGE_WIDTH/2, 0, PAGE_WIDTH/2, PAGE_HEIGHT)
         for lines_w in width_line:
             for lines_h in height_line:
+                # horizontal lines
                 can.line(0, lines_w - font_size/4, PAGE_WIDTH, lines_w - font_size/4)
-                can.line(lines_h, 0, lines_h, PAGE_HEIGHT)
+                # vertical lines
+                # can.line(lines_h, 0, lines_h, PAGE_HEIGHT)
 
     drawLine(can)
 
@@ -89,10 +98,10 @@ def generate_pdf(names):
             setCan(can)
             drawLine(can)
             y_counter = 0
-        if can.stringWidth(text[0]) > height_array[0] - 20:
+        if can.stringWidth(text[0]) > PAGE_WIDTH/2 - 40:
             text_array = text[0].split(" ", 1)
-            if can.stringWidth(text_array[1]) > height_array[0] - 20:
-                    can.setFont(font, font_size/1.5)
+            if can.stringWidth(text_array[1]) > PAGE_WIDTH/2 - 40:
+                    can.setFont(font, font_size/1.2)
 
             can.drawString(height_array[y_counter] - can.stringWidth(text_array[0])/2, width_array[x_counter] - width_array[0]/spacing, text_array[0])
             can.drawString(height_array[y_counter] - can.stringWidth(text_array[1])/2, width_array[x_counter] + width_array[0]/spacing, text_array[1])
